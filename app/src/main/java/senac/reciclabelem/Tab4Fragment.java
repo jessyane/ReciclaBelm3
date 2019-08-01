f5502ab6a2302143bd110957a8c4d12005392f13
jessyane <jessyanealmeida99@gmail.com>
2019-08-01 00:53:01 -0300

    Google maps!

--- app/src/main/java/senac/reciclabelem/Tab4Fragment.java
+++ app/src/main/java/senac/reciclabelem/Tab4Fragment.java
@@ -3,11 +3,15 @@ package senac.reciclabelem;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
 
 /**
@@ -15,11 +19,20 @@ import android.view.ViewGroup;
  */
 public class Tab4Fragment extends Fragment {
 
+    private GoogleMap mMap;
 
     public Tab4Fragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
