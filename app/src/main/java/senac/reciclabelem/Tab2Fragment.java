bb0cace37cedd2155c5b9025ac69eb89e8aa7ad9
Lucas Noronha Ferreira <lucasmistery12@gmail.com>
2019-08-01 00:41:39 -0300

    Internet OK

--- app/src/main/java/senac/reciclabelem/Tab2Fragment.java
+++ app/src/main/java/senac/reciclabelem/Tab2Fragment.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 
 /**
@@ -27,5 +31,6 @@ public class Tab2Fragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab2, container, false);
     }
+
 
 }
