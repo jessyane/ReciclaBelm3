af1ba806bf7e054b3aa2bdba0bf122b9bf31db4b
jessyane <jessyanealmeida99@gmail.com>
2019-08-03 00:55:10 -0300

    adcionei a localização

--- app/src/main/java/senac/reciclabelem/MapsActivity.java
+++ app/src/main/java/senac/reciclabelem/MapsActivity.java
@@ -1,8 +1,16 @@
 package senac.reciclabelem;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +18,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,9 +35,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
-    /**
+       /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
@@ -35,9 +44,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setTrafficEnabled(true);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
--- app/src/main/java/senac/reciclabelem/Tab4Fragment.java
+++ app/src/main/java/senac/reciclabelem/Tab4Fragment.java
@@ -45,7 +45,15 @@ public class Tab4Fragment extends SupportMapFragment implements OnMapReadyCallba
 
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng belém = new LatLng(-1.4615468,-48.4920082);
-        mMap.addMarker(new MarkerOptions().position(belém).title("Belém"));
+
+        mMap.addMarker(new MarkerOptions().position(belém).title("marker in Belém"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4574703,-48.4962724)).title("EcoArt"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4601204,-48.4907819)).title("Cooperativa Filhos Do Sol & Ascapel"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4678905,-48.4917843)).title("Galpão CONCAVES"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4035158,-48.4971134)).title("Jorge Reciclagem"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4495951,-48.4927807)).title("Só O Senhor e Deus Comércio"));
+
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(belém, 14.5f));
     }
 }
