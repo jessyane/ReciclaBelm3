63e27db9f729b8b3bda196877da94e8810b0b923
Lucas Noronha <lucasmistert12@gmail.com>
2019-08-05 18:25:40 -0300

    Material Design 1.0

--- app/src/main/java/senac/reciclabelem/Tab1Fragment.java
+++ app/src/main/java/senac/reciclabelem/Tab1Fragment.java
@@ -1,13 +1,17 @@
 package senac.reciclabelem;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 /**
@@ -17,15 +21,23 @@ public class Tab1Fragment extends Fragment {
 
 
     public Tab1Fragment() {
-        // Required empty public constructor
-    }
 
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
-    }
 
+        View view = inflater.inflate(R.layout.fragment_tab1,
+                container, false);
+        ImageButton button = (ImageButton) view.findViewById(R.id.btn_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getActivity(), Tab0Fragment.class);
+                getActivity().startActivity(myIntent);
+            }
+        });
+        return view;
+    }
 }
