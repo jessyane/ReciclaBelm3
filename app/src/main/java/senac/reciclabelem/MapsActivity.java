2e05aa5087d78487dab4889fcdee2d787848d1e9
jessyane <jessyanealmeida99@gmail.com>
2019-08-06 00:03:18 -0300

    nova atualização do mapa

--- app/src/main/java/senac/reciclabelem/MapsActivity.java
+++ app/src/main/java/senac/reciclabelem/MapsActivity.java
@@ -4,13 +4,17 @@ import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -65,5 +69,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+
+            try{
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                LocationListener locationListener = new LocationListener() {
+                    public void onLocationChanged(Location location) {
+                        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in local position"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+                    }
+
+                    public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                    public void onProviderEnabled(String provider) { }
+
+                    public void onProviderDisabled(String provider) { }
+                };
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            }catch(SecurityException ex){
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+
+
+            } }
 }
--- app/src/main/java/senac/reciclabelem/Tab4Fragment.java
+++ app/src/main/java/senac/reciclabelem/Tab4Fragment.java
@@ -52,6 +52,31 @@ public class Tab4Fragment extends SupportMapFragment implements OnMapReadyCallba
         mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4678905,-48.4917843)).title("Galpão CONCAVES"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4035158,-48.4971134)).title("Jorge Reciclagem"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4495951,-48.4927807)).title("Só O Senhor e Deus Comércio"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4605302,-48.4903761)).title("Viggiano Rei do Ferro Velho"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4277733,-48.5027262)).title("Instituto Alachaster"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4580835,-48.5045545)).title("Sucataria Taco"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4506111,-48.4867292)).title("Sucataria José"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4488924,-48.4945159)).title("JVR-Sucata"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4506111,-48.4867292)).title("Sucata Vasconcelos"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4303321,-48.5042176)).title("Sucataria 2 amigos"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4249461,-48.4890269)).title("Riopel"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.39507,-48.497911)).title("Transforma"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4589147,-48.4882546)).title("reciclagem luz do amanha"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.453033,-48.4908251)).title("Pretória - Formação de Vigilantes"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4257569,-48.5038247)).title("SUCATÃO DO BETO "));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4520054,-48.4896194)).title("Brechó da Celina"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4497907,-48.5011763)).title("Ong noolhar"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4549346,-48.5022275)).title("Coleta de Entulho Belém Hidrovac"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4560648,-48.4914237)).title("DRES SESAN"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4270648,-48.4927201)).title("CRS Resíduos"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-1.4562207,-48.4914341)).title("Servmix"));
+
+
+
+
+
+
+
 
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(belém, 14.5f));
